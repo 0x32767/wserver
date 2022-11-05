3f435de39609203d48fe16265e43dbc12174e0c0
badUsername123456 <95779166+0x32767@users.noreply.github.com>
2022-11-05 16:48:03 +0000

    websockets working

--- PYFSD/html.py
+++ PYFSD/html.py
@@ -1,6 +1,5 @@
 from jinja2.sandbox import SandboxedEnvironment
-from base64 import b64encode
-from hashlib import sha1
+from websockets.utils import accept_key
 
 env = SandboxedEnvironment()
 
@@ -9,22 +8,19 @@ def res(html: str, status=(200, "OK")) -> str:
     return f"HTTP/1.0 {status[0]} {status[1]}\n\n" + html
 
 
-def render_template(
-    path: str, *args, **kwargs
-) -> str:
+def render_template(path: str, *args, **kwargs) -> str:
 
     with open(path, "r") as f:
         return res(env.from_string(f.read()).render(*args, **kwargs))
 
 
+def _decorate_ws(key: str) -> str:
+    return f"""HTTP/1.1 101 Switching Protocols\r\nUpgrade: websocket\r\nConnection: Upgrade\r\nSec-WebSocket-Accept: {key}\r\nSec-WebSocket-Protocol: chat"""
+
+
 # an incoming websocket will give some random b64 int, we
 # need to add some magic number to it and then sha1 it and
 # return it as a b64
-def accept_handshake(key: str, magic: int):
+def accept_handshake(key: str):
     # hex to base 64
-    return b64encode(bytes.fromhex(
-        # this is the important part, this will add a magic
-        # number and then sha1 it, this is just how
-        # websockets work
-        sha1(int(key, 64) + magic).hexdigest()
-    )).decode()
+    return _decorate_ws(accept_key(key))
