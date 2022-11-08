ef553de586439284f153ee2106d0633c133e603b
badUsername123456 <95779166+0x32767@users.noreply.github.com>
2022-11-08 21:57:27 +0000

    fixed fustrating bug

--- PYFSD/html.py
+++ PYFSD/html.py
@@ -8,8 +8,10 @@ def res(html: str, status=(200, "OK")) -> str:
 class GenericHTMLElement:
     def __init__(self, name: str, self_closing: bool = False, *args, **kwargs) -> None:
         # this genorates the id="some-name" part of an element
-        self.attrs = " ".join([str(k) + "=\"" + str(v) + "\" " for k, v in kwargs.items()]) +\
-            " ".join(args) # the arrgs are for example hidden
+        self.attrs = " ".join(
+            [str(k) + '="' + str(v) + '" ' for k, v in kwargs.items()]
+        ) + " ".join(args)
+        # the arrgs are for example hidden
 
         # together this makes up the:
         # hidden id="123"
@@ -24,7 +26,7 @@ class GenericHTMLElement:
 
     def render(self, element) -> str:
         if isinstance(element, str):
-            return element
+            return f"<{self.name} {self.attrs}>\n{element}\n</{self.name}>"
 
         if self.self_closing:
             return f"<{self.name} {self.attrs} />"
@@ -33,165 +35,147 @@ class GenericHTMLElement:
         return f"<{self.name} {self.attrs}>\n{inner}\n</{self.name}>"
 
 
-class HTMLMetta(type):
-    # this is a dict full of the html tags, the key is the name, and the value is if the
-    # tag is self closing
-    html_tags = {
-        "html": False,
-        "abbr": False,
-        "acronym": False,
-        "address": False,
-        "a": False,
-        "applet": False,
-        "area": False,
-        "artcile": False,
-        "aside": False,
-        "audio": False,
-        "base": True,
-        "basefont": True,
-        "bdi": False,
-        "bdo": False,
-        "bgsound ": True,
-        "big": False,
-        "blockquote": False,
-        "body": False,
-        "b": False,
-        "br": True,
-        "button": False,
-        "caption": False,
-        "canvas": False,
-        "center": False,
-        "cite": False,
-        "code": False,
-        "colgroup": False,
-        "col": True,
-        "data": False,
-        "datalist": False,
-        "dd": False,
-        "define": False,
-        "delete": False,
-        "details": False,
-        "dialog ": False,
-        "dir": False,
-        "div": False,
-        "dl": False,
-        "dt": False,
-        "embed": True,
-        "fieldset": False,
-        "figcaption": False,
-        "figure": False,
-        "font": False,
-        "footer": False,
-        "form": False,
-        "frame": True,
-        "frameset": True,
-        "head": False,
-        "header": False,
-        "h1": False,
-        "h2": False,
-        "h3": False,
-        "h4": False,
-        "h5": False,
-        "h6": False,
-        "hgroup": False,
-        "hr": True,
-        "html": False,
-        "iframe": False,
-        "img": True,
-        "input": True,
-        "ins": False,
-        "isindex": True,
-        "i": False,
-        "kpd": False,
-        "keygen": True,
-        "label": False,
-        "legend": False,
-        "li": False,
-        "main": False,
-        "mark": False,
-        "marquee": False,
-        "menuitem": False,
-        "meta": True,
-        "meter": False,
-        "nav": False,
-        "bobr": False,
-        "noembed": False,
-        "noscript": False,
-        "object": False,
-        "optgroup": False,
-        "option": False,
-        "output": False,
-        "p": False,
-        "param": False,
-        "em": False,
-        "pre": False,
-        "progress": False,
-        "q": False,
-        "rp": False,
-        "rt": False,
-        "ruby": False,
-        "samp": False,
-        "script": False,
-        "section": False,
-        "small": False,
-        "source": False,
-        "spacer": False,
-        "span": False,
-        "strike": False,
-        "strong": False,
-        "style": False,
-        "sub": False,
-        "summary": False,
-        "svg": True,
-        "table": False,
-        "tbody": False,
-        "td": False,
-        "template": False,
-        "tfoot": False,
-        "th": False,
-        "thead": False,
-        "time": False,
-        "title": False,
-        "tr": False,
-        "track": True,
-        "tt": False,
-        "u": False,
-        "var": False,
-        "video": False,
-        "wbr": True,
-        "xmp": False
-    }
-
-    def __new__(cls, name: str, bases: Tuple[Any], attrs: Dict[str, Any]) -> None:
-        # the key is the name of the the tag and the value is if the tag is self closing
-        for k, v in cls.html_tags:
-            # the atters are the functions and attribvutes the class has
-            # here we are creating a new function, this function has access
-            # to the namespace
-            attrs[k] = lambda s, *args, **keargs:\
-                GenericHTMLElement(
-                    # This will define the the value of the element
-                    name=k,
-                    self_closing=v
-                )
-
-        """
-        Although this aproach is a bit arquard it is much more eficiant than writing a class
-        with all html attributes. The metaclass above is equivilent to the following code:
-        >>> class HTML:
-        ...     def a(self, *args, **kwargs):
-        ...         return GenericHTMLElement("a", False, *args, **kwargs)
-        ...
-        ...     def div(self, *args, **kwargs):
-        ...         return GenericHTMLElement("div", False, *args, **kwargs)
-        ...
-        ...     def img(self, *args, **kwargs):
-        ...         return GenericHTMLElement("img", True, *args, **kwargs)
-        ... # This parrern would continue with the mremaining of the html elements,
-        """
-
-        return type(name, bases, attrs)
-
-
-class HTML(metaclass=HTMLMetta):
-    ...
+html = lambda *args, **kwargs: GenericHTMLElement("html", False, *args, **kwargs)
+abbr = lambda *args, **kwargs: GenericHTMLElement("abbr", False, *args, **kwargs)
+acronym = lambda *args, **kwargs: GenericHTMLElement("acronym", False, *args, **kwargs)
+address = lambda *args, **kwargs: GenericHTMLElement("address", False, *args, **kwargs)
+a = lambda *args, **kwargs: GenericHTMLElement("a", False, *args, **kwargs)
+applet = lambda *args, **kwargs: GenericHTMLElement("applet", False, *args, **kwargs)
+area = lambda *args, **kwargs: GenericHTMLElement("area", False, *args, **kwargs)
+artcile = lambda *args, **kwargs: GenericHTMLElement("artcile", False, *args, **kwargs)
+aside = lambda *args, **kwargs: GenericHTMLElement("aside", False, *args, **kwargs)
+audio = lambda *args, **kwargs: GenericHTMLElement("audio", False, *args, **kwargs)
+base = lambda *args, **kwargs: GenericHTMLElement("base", True, *args, **kwargs)
+basefont = lambda *args, **kwargs: GenericHTMLElement("basefont", True, *args, **kwargs)
+bdi = lambda *args, **kwargs: GenericHTMLElement("bdi", False, *args, **kwargs)
+bdo = lambda *args, **kwargs: GenericHTMLElement("bdo", False, *args, **kwargs)
+bgsound = lambda *args, **kwargs: GenericHTMLElement("bgsound ", True, *args, **kwargs)
+big = lambda *args, **kwargs: GenericHTMLElement("big", False, *args, **kwargs)
+blockquote = lambda *args, **kwargs: GenericHTMLElement(
+    "blockquote", False, *args, **kwargs
+)
+body = lambda *args, **kwargs: GenericHTMLElement("body", False, *args, **kwargs)
+b = lambda *args, **kwargs: GenericHTMLElement("b", False, *args, **kwargs)
+br = lambda *args, **kwargs: GenericHTMLElement("br", True, *args, **kwargs)
+button = lambda *args, **kwargs: GenericHTMLElement("button", False, *args, **kwargs)
+caption = lambda *args, **kwargs: GenericHTMLElement("caption", False, *args, **kwargs)
+canvas = lambda *args, **kwargs: GenericHTMLElement("canvas", False, *args, **kwargs)
+center = lambda *args, **kwargs: GenericHTMLElement("center", False, *args, **kwargs)
+cite = lambda *args, **kwargs: GenericHTMLElement("cite", False, *args, **kwargs)
+code = lambda *args, **kwargs: GenericHTMLElement("code", False, *args, **kwargs)
+colgroup = lambda *args, **kwargs: GenericHTMLElement(
+    "colgroup", False, *args, **kwargs
+)
+col = lambda *args, **kwargs: GenericHTMLElement("col", True, *args, **kwargs)
+data = lambda *args, **kwargs: GenericHTMLElement("data", False, *args, **kwargs)
+datalist = lambda *args, **kwargs: GenericHTMLElement(
+    "datalist", False, *args, **kwargs
+)
+dd = lambda *args, **kwargs: GenericHTMLElement("dd", False, *args, **kwargs)
+define = lambda *args, **kwargs: GenericHTMLElement("define", False, *args, **kwargs)
+delete = lambda *args, **kwargs: GenericHTMLElement("delete", False, *args, **kwargs)
+details = lambda *args, **kwargs: GenericHTMLElement("details", False, *args, **kwargs)
+dialog = lambda *args, **kwargs: GenericHTMLElement("dialog ", False, *args, **kwargs)
+dir = lambda *args, **kwargs: GenericHTMLElement("dir", False, *args, **kwargs)
+div = lambda *args, **kwargs: GenericHTMLElement("div", False, *args, **kwargs)
+dl = lambda *args, **kwargs: GenericHTMLElement("dl", False, *args, **kwargs)
+dt = lambda *args, **kwargs: GenericHTMLElement("dt", False, *args, **kwargs)
+embed = lambda *args, **kwargs: GenericHTMLElement("embed", True, *args, **kwargs)
+fieldset = lambda *args, **kwargs: GenericHTMLElement(
+    "fieldset", False, *args, **kwargs
+)
+figcaption = lambda *args, **kwargs: GenericHTMLElement(
+    "figcaption", False, *args, **kwargs
+)
+figure = lambda *args, **kwargs: GenericHTMLElement("figure", False, *args, **kwargs)
+font = lambda *args, **kwargs: GenericHTMLElement("font", False, *args, **kwargs)
+footer = lambda *args, **kwargs: GenericHTMLElement("footer", False, *args, **kwargs)
+form = lambda *args, **kwargs: GenericHTMLElement("form", False, *args, **kwargs)
+frame = lambda *args, **kwargs: GenericHTMLElement("frame", True, *args, **kwargs)
+frameset = lambda *args, **kwargs: GenericHTMLElement("frameset", True, *args, **kwargs)
+head = lambda *args, **kwargs: GenericHTMLElement("head", False, *args, **kwargs)
+header = lambda *args, **kwargs: GenericHTMLElement("header", False, *args, **kwargs)
+h1 = lambda *args, **kwargs: GenericHTMLElement("h1", False, *args, **kwargs)
+h2 = lambda *args, **kwargs: GenericHTMLElement("h2", False, *args, **kwargs)
+h3 = lambda *args, **kwargs: GenericHTMLElement("h3", False, *args, **kwargs)
+h4 = lambda *args, **kwargs: GenericHTMLElement("h4", False, *args, **kwargs)
+h5 = lambda *args, **kwargs: GenericHTMLElement("h5", False, *args, **kwargs)
+h6 = lambda *args, **kwargs: GenericHTMLElement("h6", False, *args, **kwargs)
+hgroup = lambda *args, **kwargs: GenericHTMLElement("hgroup", False, *args, **kwargs)
+hr = lambda *args, **kwargs: GenericHTMLElement("hr", True, *args, **kwargs)
+html = lambda *args, **kwargs: GenericHTMLElement("html", False, *args, **kwargs)
+iframe = lambda *args, **kwargs: GenericHTMLElement("iframe", False, *args, **kwargs)
+img = lambda *args, **kwargs: GenericHTMLElement("img", True, *args, **kwargs)
+input = lambda *args, **kwargs: GenericHTMLElement("input", True, *args, **kwargs)
+ins = lambda *args, **kwargs: GenericHTMLElement("ins", False, *args, **kwargs)
+isindex = lambda *args, **kwargs: GenericHTMLElement("isindex", True, *args, **kwargs)
+i = lambda *args, **kwargs: GenericHTMLElement("i", False, *args, **kwargs)
+kpd = lambda *args, **kwargs: GenericHTMLElement("kpd", False, *args, **kwargs)
+keygen = lambda *args, **kwargs: GenericHTMLElement("keygen", True, *args, **kwargs)
+label = lambda *args, **kwargs: GenericHTMLElement("label", False, *args, **kwargs)
+legend = lambda *args, **kwargs: GenericHTMLElement("legend", False, *args, **kwargs)
+li = lambda *args, **kwargs: GenericHTMLElement("li", False, *args, **kwargs)
+main = lambda *args, **kwargs: GenericHTMLElement("main", False, *args, **kwargs)
+mark = lambda *args, **kwargs: GenericHTMLElement("mark", False, *args, **kwargs)
+marquee = lambda *args, **kwargs: GenericHTMLElement("marquee", False, *args, **kwargs)
+menuitem = lambda *args, **kwargs: GenericHTMLElement(
+    "menuitem", False, *args, **kwargs
+)
+meta = lambda *args, **kwargs: GenericHTMLElement("meta", True, *args, **kwargs)
+meter = lambda *args, **kwargs: GenericHTMLElement("meter", False, *args, **kwargs)
+nav = lambda *args, **kwargs: GenericHTMLElement("nav", False, *args, **kwargs)
+bobr = lambda *args, **kwargs: GenericHTMLElement("bobr", False, *args, **kwargs)
+noembed = lambda *args, **kwargs: GenericHTMLElement("noembed", False, *args, **kwargs)
+noscript = lambda *args, **kwargs: GenericHTMLElement(
+    "noscript", False, *args, **kwargs
+)
+object = lambda *args, **kwargs: GenericHTMLElement("object", False, *args, **kwargs)
+optgroup = lambda *args, **kwargs: GenericHTMLElement(
+    "optgroup", False, *args, **kwargs
+)
+option = lambda *args, **kwargs: GenericHTMLElement("option", False, *args, **kwargs)
+output = lambda *args, **kwargs: GenericHTMLElement("output", False, *args, **kwargs)
+p = lambda *args, **kwargs: GenericHTMLElement("p", False, *args, **kwargs)
+param = lambda *args, **kwargs: GenericHTMLElement("param", False, *args, **kwargs)
+em = lambda *args, **kwargs: GenericHTMLElement("em", False, *args, **kwargs)
+pre = lambda *args, **kwargs: GenericHTMLElement("pre", False, *args, **kwargs)
+progress = lambda *args, **kwargs: GenericHTMLElement(
+    "progress", False, *args, **kwargs
+)
+q = lambda *args, **kwargs: GenericHTMLElement("q", False, *args, **kwargs)
+rp = lambda *args, **kwargs: GenericHTMLElement("rp", False, *args, **kwargs)
+rt = lambda *args, **kwargs: GenericHTMLElement("rt", False, *args, **kwargs)
+ruby = lambda *args, **kwargs: GenericHTMLElement("ruby", False, *args, **kwargs)
+samp = lambda *args, **kwargs: GenericHTMLElement("samp", False, *args, **kwargs)
+script = lambda *args, **kwargs: GenericHTMLElement("script", False, *args, **kwargs)
+section = lambda *args, **kwargs: GenericHTMLElement("section", False, *args, **kwargs)
+small = lambda *args, **kwargs: GenericHTMLElement("small", False, *args, **kwargs)
+source = lambda *args, **kwargs: GenericHTMLElement("source", False, *args, **kwargs)
+spacer = lambda *args, **kwargs: GenericHTMLElement("spacer", False, *args, **kwargs)
+span = lambda *args, **kwargs: GenericHTMLElement("span", False, *args, **kwargs)
+strike = lambda *args, **kwargs: GenericHTMLElement("strike", False, *args, **kwargs)
+strong = lambda *args, **kwargs: GenericHTMLElement("strong", False, *args, **kwargs)
+style = lambda *args, **kwargs: GenericHTMLElement("style", False, *args, **kwargs)
+sub = lambda *args, **kwargs: GenericHTMLElement("sub", False, *args, **kwargs)
+summary = lambda *args, **kwargs: GenericHTMLElement("summary", False, *args, **kwargs)
+svg = lambda *args, **kwargs: GenericHTMLElement("svg", True, *args, **kwargs)
+table = lambda *args, **kwargs: GenericHTMLElement("table", False, *args, **kwargs)
+tbody = lambda *args, **kwargs: GenericHTMLElement("tbody", False, *args, **kwargs)
+td = lambda *args, **kwargs: GenericHTMLElement("td", False, *args, **kwargs)
+template = lambda *args, **kwargs: GenericHTMLElement(
+    "template", False, *args, **kwargs
+)
+tfoot = lambda *args, **kwargs: GenericHTMLElement("tfoot", False, *args, **kwargs)
+th = lambda *args, **kwargs: GenericHTMLElement("th", False, *args, **kwargs)
+thead = lambda *args, **kwargs: GenericHTMLElement("thead", False, *args, **kwargs)
+time = lambda *args, **kwargs: GenericHTMLElement("time", False, *args, **kwargs)
+title = lambda *args, **kwargs: GenericHTMLElement("title", False, *args, **kwargs)
+tr = lambda *args, **kwargs: GenericHTMLElement("tr", False, *args, **kwargs)
+track = lambda *args, **kwargs: GenericHTMLElement("track", True, *args, **kwargs)
+tt = lambda *args, **kwargs: GenericHTMLElement("tt", False, *args, **kwargs)
+u = lambda *args, **kwargs: GenericHTMLElement("u", False, *args, **kwargs)
+var = lambda *args, **kwargs: GenericHTMLElement("var", False, *args, **kwargs)
+video = lambda *args, **kwargs: GenericHTMLElement("video", False, *args, **kwargs)
+wbr = lambda *args, **kwargs: GenericHTMLElement("wbr", True, *args, **kwargs)
+xmp = lambda *args, **kwargs: GenericHTMLElement("xmp", False, *args, **kwargs)
+html = lambda *args, **kwargs: GenericHTMLElement("html", False, *args, **kwargs)
